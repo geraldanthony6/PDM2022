1ec9f9dd599c763bf08d66d725e3ac9052968fd8
geraldanthony6 <77769808+geraldanthony6@users.noreply.github.com>
2022-01-20 16:24:24 -0600

    Add files via upload
    Example2 Added

---
+++ Assignment1/example2.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  fill('rgba(255, 169, 169, 0.25)');
+  noStroke();
+  ellipse(49, 40, 55, 55);
+  
+  fill('rgba(170, 168, 255, 0.25)');
+  noStroke();
+  ellipse(30, 75, 55, 55);
+  
+  fill('rgba(167, 255, 167, 0.25)');
+  noStroke();
+  ellipse(70, 75, 55, 55);
+}
