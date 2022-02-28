f0d9df9f5d121ed36445a480e70ec7a60fff3e0e
Gerald Anthony <geraldanthony@Gerald.local>
2022-02-28 16:59:32 -0600

    Synth check

--- Synth/js/Synth.js
+++ Synth/js/Synth.js
@@ -1,21 +1,18 @@
-const synth = new Tone.PluckSynth();
-const drum = new Tone.MembraneSynth();
-const metal = new Tone.MetalSynth({
-	"frequency"  : 45 ,
-	"envelope"  : {
-		"attack"  : 0.001 ,
-		"decay"  : 0.4 ,
-		"release"  : 0.2
-	}  ,
-	"harmonicity"  : 8.5 ,
-	"modulationIndex"  : 40 ,
-	"resonance"  : 300 ,
-	"octaves"  : 1.5
+const synthDrum = new Tone.MembraneSynth({
+  "frequency"  : 45 ,
+"envelope"  : {
+  "attack"  : 0.001 ,
+  "decay"  : 0.4 ,
+  "release"  : 0.2
+}  ,
+"harmonicity"  : 8.5 ,
+"modulationIndex"  : 40 ,
+"resonance"  : 300 ,
+"octaves"  : 1.5
 });
+
 const reverb = new Tone.JCReverb(0.4).toDestination();
-synth.connect(reverb);
-drum.connect(reverb);
-metal.connect(reverb);
+synthDrum.connect(reverb);
 
 let notes = {
   'a': 'C4',
@@ -30,15 +27,12 @@ let notes = {
 
 function setup() {
   createCanvas(400, 400);
-  synth.release = 2;
+  synth.release = 1;
   synth.resonance = 0.98;
-  // synth.harmonicity.value = 1.25;
-  //play a middle 'C' for the duration of an 8th note
-  synth.triggerAttackRelease("C4", "8n");
 }
 
 function draw() {
-  background(220);
+  background(100);
 }
 
 function keyPressed() {
